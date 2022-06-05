782281f8b7f586125e8a9440620322f8368366ec
Andres <andreslindo20@gmail.com>
2022-06-04 21:52:12 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const { response } = require('express')
+const { request } = require('express')
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+const rappers = {
+    '21 savage':{
+     'age':29,
+    'birthname': 'Sheyaa Bin Abrham',
+    'birthLocation': 'London, England',
+    },
+    'chance the rapper':{
+        'age':29,
+    'birthname': 'Chanceller Bennet',
+    'birthLocation': 'Chicago, illinois',
+    },
+    'unknown':{
+        'age':"Unknown",
+    'birthname': 'unknown',
+    'birthLocation': 'unknown',
+    }
+}
+
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname, '/index.html')
+})
+
+app.get('/api/:name', (req, res)=>{
+    const rapperName = req.params.name.toLowerCase()
+    if(rappers[rapperName]){
+        response.json(rappers[rapperName])
+    }else{
+        response.json(rappers[unknown])
+    }
+    res.json(rappers)
+})
+
+
+app.listen(PORT, ()=> {
+    console.log(`The server is now running on port ${PORT}`)
+})
